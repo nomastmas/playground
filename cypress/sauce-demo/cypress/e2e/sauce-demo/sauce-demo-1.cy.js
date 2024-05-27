2c0bc5882f912d6feaadfa5e49ce8558c657181c
nomastmas <thomaswtsang@gmail.com>
2024-05-27 15:32:47 -0400

    Add cypress files

---
+++ cypress/sauce-demo/cypress/e2e/sauce-demo/sauce-demo-1.cy.js
@@ -0,0 +1,61 @@
+describe('sauce-demo', () => {
+  const standardUser = Cypress.env('standard');
+  const username = standardUser.username;
+  const password = standardUser.password;
+
+  beforeEach(() => {
+    cy.visit('/');
+    cy.get('[data-test="username"]').type(username);
+    cy.get('[data-test="password"]').type(password);
+    cy.get('[data-test="login-button"]').click();
+    cy.location('pathname').should('eq', '/inventory.html');
+  });
+
+  it('asserts user is logged in', () => {
+    cy.location('pathname').should('eq', '/inventory.html');
+    cy.get('.app_logo').should('have.text', 'Swag Labs');
+  });
+
+  it('asserts user can add an item and checkout', () => {
+    cy.get('[data-test="add-to-cart-sauce-labs-backpack"]').click()
+    cy.get('[data-test="shopping-cart-link"]').click()
+
+    cy.location('pathname').should('eq', '/cart.html')
+    cy.get('[data-test="checkout"]').click()
+
+    cy.location('pathname').should('eq', '/checkout-step-one.html')
+    cy.get('[data-test="firstName"]').type('foo')
+    cy.get('[data-test="lastName"]').type('bar')
+    cy.get('[data-test="postalCode"]').type('12345')
+    cy.get('[data-test="continue"]').click()
+
+    cy.location('pathname').should('eq', '/checkout-step-two.html')
+    cy.get('[data-test="finish"]').click()
+
+    cy.location('pathname').should('eq', '/checkout-complete.html')
+    cy.get('[data-test="complete-header"]').should('have.text', 'Thank you for your order!')
+  });
+
+  it('asserts user can add multiple items, remove some items, and checkout', () => {
+    cy.get('[data-test="add-to-cart-sauce-labs-backpack"]').click()
+    cy.get('[data-test="add-to-cart-sauce-labs-bike-light"]').click()
+    cy.get('[data-test="add-to-cart-sauce-labs-bolt-t-shirt"]').click()
+    cy.get('[data-test="add-to-cart-sauce-labs-fleece-jacket"]').click()
+    cy.get('[data-test="shopping-cart-link"]').click()
+    cy.get('[data-test="checkout"]').click()
+
+    cy.location('pathname').should('eq', '/checkout-step-one.html')
+    cy.get('[data-test="firstName"]').type('foo')
+    cy.get('[data-test="lastName"]').type('bar')
+    cy.get('[data-test="postalCode"]').type('12345')
+    cy.get('[data-test="continue"]').click()
+
+    cy.location('pathname').should('eq', '/checkout-step-two.html')
+    cy.get('[data-test="finish"]').click()
+
+    cy.location('pathname').should('eq', '/checkout-complete.html')
+    cy.get('[data-test="complete-header"]').should('have.text', 'Thank you for your order!')
+  });
+
+
+});
